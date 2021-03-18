b542ee909a2eb6de598841cd227c198fb0bd5e92
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-18 05:49:45 +0200

    navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,20 +10,20 @@ export class NavbarComponent implements OnInit {
 
   constructor(private router: Router) { }
  action:string ="Sign In"
- iconClass:string="fas fa-sign-in-alt"
+ iconClass:string="fas fa-sign-in-alt fa-lg"
  root:string="/register"
   ngOnInit(): void {
     this.checkToken()
   }
   onClick(){
     
-    
-      if(this.action="Sign In"){
-      this.root="/register"
+   console.log(this.action)
+      if(this.action=="Profile"){
+      this.root="/profile"
     }
     else
     {
-      this.root="/profile"
+      this.root="/register"
     
     }
   }
@@ -31,11 +31,11 @@ export class NavbarComponent implements OnInit {
     if(localStorage.getItem('Token')!=null||localStorage.getItem('Token')!=undefined)
     {
       this.action='Profile'
-      this.iconClass='far fa-user-circle'
+      this.iconClass='far fa-user-circle fa-lg'
     }
     else{
       this.action="Sing In"
-      this.iconClass="fas fa-sign-in-alt"
+      this.iconClass="fas fa-sign-in-alt fa-lg"
     }
   }
   logOut(){
