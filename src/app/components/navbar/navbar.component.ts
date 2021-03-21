ad15311e102daf86aba3b613ac95adae9a16e0d7
nahlaFathy <nehal.fathy.fci.scu@gmail.com>
2021-03-21 05:49:01 +0200

    images

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -23,7 +23,7 @@ export class NavbarComponent implements OnInit {
         console.log("emitted")
         this.checkToken();    
         this.user=true
-        this.isAdmin=true
+        this.isAdmin=false
       });    
     }   
   }
